62ae5fb730adc1624533de3305531c7aa2820ccb
Zac <13089@holbertonstudents.com.au>
2026-07-29 16:15:53 +1000

    added floor code

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module provides a function that returns the floor of a float."""
+
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of a floating-point number."""
+    return math.floor(n)
