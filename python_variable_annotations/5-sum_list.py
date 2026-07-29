83136c41468a6b55dd3bd4c09c3ebb3e6ed8e4be
Zac <13089@holbertonstudents.com.au>
2026-07-29 16:29:38 +1000

    added code for sum list file

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This module provides a functrion that returns the suym of a list of floats."""
+
+from typing import List
+
+
+def sum_list(input_list: List[float]) -> float:
+    """Return the sum of all floats in a list."""
+    return sum(input_list)
