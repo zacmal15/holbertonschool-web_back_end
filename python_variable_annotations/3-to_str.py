454bed77be4b06442de2091ebf27f60caa57a5d5
Zac <13089@holbertonstudents.com.au>
2026-07-29 16:24:38 +1000

    added float to string code

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module provides a function that converts a float to a string."""
+
+
+def to_str(n: float) -> str:
+    """Return the string representation of a float."""
+    return str(n)
