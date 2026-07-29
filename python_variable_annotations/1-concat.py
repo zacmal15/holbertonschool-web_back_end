3448d57293555442e9544ce61875f8d5c1032cf1
Zac <13089@holbertonstudents.com.au>
2026-07-29 14:09:11 +1000

    added concatenating code

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module provides a function for concatenating two strings."""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return the concatenation of two strings."""
+    return str1 + str2
