163240fe4b0aeb8554c4ca1ae59fbe61b24d55b4
Zac <13089@holbertonstudents.com.au>
2026-07-29 14:04:56 +1000

    added add function

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""This module provides a function for adding two floating-point numbers."""
+
+def add(a: float, b: float) -> float:
+    """Return the sum of two floating-point numbers."""
+    return a + b
