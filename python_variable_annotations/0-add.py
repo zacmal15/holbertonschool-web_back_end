b66233f79c7d16cd5ff907b2c2ef2a4ff20a7cf2
Zac <13089@holbertonstudents.com.au>
2026-07-29 14:07:35 +1000

    fix pycodestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """This module provides a function for adding two floating-point numbers."""
 
+
 def add(a: float, b: float) -> float:
     """Return the sum of two floating-point numbers."""
     return a + b
