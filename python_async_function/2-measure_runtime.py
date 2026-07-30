615a764304bf780fb81f2bc2daff1fbe6c77cbd5
Zac <13089@holbertonstudents.com.au>
2026-07-30 16:52:37 +1000

    added runtime code

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""This module measures the avg runtime of wait_n."""
+
+import asyncio
+import time
+
+wait_n = __import__("1-concurrent_coroutines").wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """Measures the avg execution time of wait_n."""
+
+    start = time.time()
+
+    asyncio.run(wait_n(n, max_delay))
+
+    end = time.time()
+
+    return (end - start) / n
