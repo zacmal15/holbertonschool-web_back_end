418f5e5055141aa53fbbf97ca511d373b37838f7
Zac <13089@holbertonstudents.com.au>
2026-07-30 17:37:38 +1000

    fix

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,5 +1,5 @@
- #!/usr/bin/env python3
-"""This module runs multiple async tasks concurrently"""
+#!/usr/bin/env python3
+"""This module runs multiple asynchronous tasks concurrently."""
 
 import asyncio
 from typing import List
@@ -8,10 +8,10 @@ task_wait_random = __import__("3-tasks").task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """Run task_wait_rnaomd n times and return delays in completion order."""
+    """Run task_wait_random n times and return delays in completion order."""
 
-    tasks = [task_wait_random(max_delay) for _ in range (n)]
-    delays = []
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays: List[float] = []
 
     for task in asyncio.as_completed(tasks):
         delays.append(await task)
