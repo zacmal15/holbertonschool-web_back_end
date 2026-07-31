bb01f358f3c90ad9a5f4ea0dcf75c35251b8cba2
Zac <13089@holbertonstudents.com.au>
2026-07-31 15:27:07 +1000

    fix

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-"""Module that measures the runtime of async comprehensions."""
+"""Module that measures asynchronous comprehension runtime."""
 
 import asyncio
 import time
 
-async_comprehension = __import__('1-async_comprehension').async_comprehension
+async_comprehension = __import__(
+    '1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Measure the runtime of four async comprehensions."""
-    start = time.time()
+    """Execute four asynchronous comprehensions and return their runtime."""
+    start_time = time.time()
 
     await asyncio.gather(
         async_comprehension(),
@@ -18,4 +19,4 @@ async def measure_runtime() -> float:
         async_comprehension()
     )
 
-    return time.time() - start
+    return time.time() - start_time
