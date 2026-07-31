82e0f2f346154babeee66b52d8deb0ab663d87af
Zac <13089@holbertonstudents.com.au>
2026-07-31 14:45:40 +1000

    fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""This module provides an asynchronous generator."""
+"""This module defines an asynchronous generator."""
 
 import asyncio
 import random
@@ -7,7 +7,7 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Yield 10 random floating-point numbers asynchronously."""
+    """Yield ten random numbers between zero and ten."""
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0, 10)
+        yield random.random() * 10
