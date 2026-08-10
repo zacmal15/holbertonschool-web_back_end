ebbb9f4e82e7055de0b0efa9983358fd3c76ac3b
Zac <13089@holbertonstudents.com.au>
2026-08-10 16:06:32 +1000

    added helper function code

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Module containing a helper function for pagination."""
+
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Return the start and end indexes for a pagination request."""
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
