08bbe590130db0b6cc10942989c0246a75bba3bd
Zac <13089@holbertonstudents.com.au>
2026-08-12 14:49:29 +1000

    fix?????

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -8,17 +8,14 @@ if __name__ == "__main__":
     client = MongoClient("mongodb://127.0.0.1:27017")
     collection = client.logs.nginx
 
-    print("{} logs".format(collection.count_documents({})))
+    print("{} logs".format(collection.estimated_document_count()))
     print("Methods:")
 
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
-    for method in methods:
+    for method in ["GET", "POST", "PUT", "PATCH", "DELETE"]:
         count = collection.count_documents({"method": method})
         print("\tmethod {}: {}".format(method, count))
 
-    status = collection.count_documents({
-        "method": "GET",
-        "path": "/status"
-    })
-    print("{} status check".format(status))
+    status_count = collection.count_documents(
+        {"method": "GET", "path": "/status"}
+    )
+    print("{} status check".format(status_count))
