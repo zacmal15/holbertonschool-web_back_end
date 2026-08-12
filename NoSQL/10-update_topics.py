6474418a4e5b53c43a4e8f2595f28295578f9235
Zac <13089@holbertonstudents.com.au>
2026-08-12 14:07:00 +1000

    typo

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -2,7 +2,7 @@
 """Module for updating school topics in mongodb"""
 
 
-def updatee_topics(mongo_collection, topics):
+def update_topics(mongo_collection, topics):
     """Update topics of all schools with matching names."""
     mongo_collection.update_many(
         {"name": name},
