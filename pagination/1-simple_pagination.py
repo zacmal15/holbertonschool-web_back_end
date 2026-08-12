1d9c8582b65f9910a52570c206f00598ace400c4
Zac <13089@holbertonstudents.com.au>
2026-08-12 16:35:22 +1000

    upload

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -1,29 +1,30 @@
 #!/usr/bin/env python3
-"""Module for simple pagination."""
-
+"""
+Pagination shall begin
+"""
 import csv
 import math
 from typing import List, Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Return the start and end indexes for a pagination range."""
+    """Takes integer arguments"""
     start = (page - 1) * page_size
     end = page * page_size
     return (start, end)
 
 
 class Server:
-    """Server class to paginate a database of popular baby names."""
-
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
-        """Initialize the server dataset cache."""
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Return the cached dataset."""
+        """Cached dataset
+        """
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -32,12 +33,10 @@ class Server:
 
         return self.__dataset
 
-    def get_page(self, page: int = 1,
-                 page_size: int = 10) -> List[List]:
-        """Return the requested page of the dataset."""
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
-
-        start, end = index_range(page, page_size)
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Alright whats going on now"""
+        assert isinstance(page, int) and page > 0, "Must be int and > 0"
+        assert isinstance(page_size, int) and page_size > 0, "Must be int > 0"
+        (start, end) = index_range(page, page_size)
 
         return self.dataset()[start:end]
