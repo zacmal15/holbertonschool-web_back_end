103164a641924dbbda23b818ca7b299e7e64af2c
Zac <13089@holbertonstudents.com.au>
2026-08-19 17:55:40 +1000

    added fifth file

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,32 @@
+import Currency from './3-currency.js';
+
+export default class Pricing {
+    constructor(amount, currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    get amount() {
+        return this._amount
+    }
+
+    set amount(value) {
+        this._amount = value;
+    }
+
+    get currency() {
+        return this._currency;
+    }
+
+    set currency(value) {
+        this._amount = value;
+    }
+
+    displayFullPrice() {
+        return `${this._amount} ${this._currency.displayFullCurrency()}`;
+    }
+
+    static convertPrice(amount, conversionRate) {
+        return amount * conversionRate;
+    }
+}
