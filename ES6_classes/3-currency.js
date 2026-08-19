1740e72fe6cee7007910f697fa52c2e72e6b882d
Zac <13089@holbertonstudents.com.au>
2026-08-19 17:48:07 +1000

    added fourth file

---
+++ ES6_classes/3-currency.js
@@ -0,0 +1,25 @@
+export default class Currency {
+    constructor(code, name) {
+        this.code = code;
+        this.name = name;
+    }
+
+    get code() {
+        return this._code;
+    }
+    set code(value) {
+        this._code = value;
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    set name(value) {
+        this._ = value;
+    }
+
+    displayFullCurrency() {
+        return `${this._name} (${this._code})`;
+    }
+}
