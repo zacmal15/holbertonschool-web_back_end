7e254af0730a9890d47698231abd5a8ba08b619f
Zac <13089@holbertonstudents.com.au>
2026-08-20 14:28:09 +1000

    added building file

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+    constructor(sqft) {
+        this._sqft = sqft;
+
+        if (this.constructor !== Building
+            && this.evacuationWarningMessage === Building.prototype.evacuationWarningMessage) {
+                throw new Error('Class extending Building must override evacuationWarningMessage');
+            }
+    }
+
+    get sqft () {
+        return this._sqft;
+    }
+
+    evacuationWarningMessage() {
+        return '';
+    }
+}
