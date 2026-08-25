ca761f2dc80c4cf7a0fbed2e068c54c686c6dac2
Zac <13089@holbertonstudents.com.au>
2026-08-25 15:38:04 +1000

    added nineth file

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,10 @@
+export default function cleanSet(set, startString) {
+    if (startString === '') {
+        return '';
+    }
+
+    return [...set]
+        .filter((value) => value.startsWith(startString))
+        .map((value) => value.slice(startString.length))
+        .join('-');
+}
