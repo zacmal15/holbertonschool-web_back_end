bc8516ed1c59c76c64c2ecaec2291dd2feaa010d
Zac <13089@holbertonstudents.com.au>
2026-08-25 15:14:44 +1000

    fix

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,3 @@
 export default function getStudentIdsSum(students) {
-    return students.reduce((total, student) => total + getStudentIdsSum.id, 0);
+    return students.reduce((total, student) => total + student.id, 0);
 }
