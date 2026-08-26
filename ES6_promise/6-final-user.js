b922260cfb6b3a0c4bec097c2610165123ff8963
Zac <13089@holbertonstudents.com.au>
2026-08-26 15:30:40 +1000

    fix

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -15,7 +15,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
 
         return {
             status: result.status,
-            value: result.reason,
+            value: result.reason.toString(),
         };
     }));
 }
