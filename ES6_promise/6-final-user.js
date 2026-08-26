b83a034dade169a21b32ae8d3f5787a56c9c79e3
Zac <13089@holbertonstudents.com.au>
2026-08-26 15:29:30 +1000

    fixed error

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,7 +1,7 @@
 import signupUser from "./4-user-promise.js";
 import uploadPhoto from "./5-photo-reject.js";
 
-export default function handleProfileSignup(firstName, lastname, fileName) {
+export default function handleProfileSignup(firstName, lastName, fileName) {
     return Promise.allSettled([
         signupUser(firstName, lastName),
         uploadPhoto(fileName),
