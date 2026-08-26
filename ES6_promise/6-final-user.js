81faa4041e31d0bf244b9d99f07e3372407b8127
Zac <13089@holbertonstudents.com.au>
2026-08-26 15:27:24 +1000

    added next file

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,21 @@
+import signupUser from "./4-user-promise.js";
+import uploadPhoto from "./5-photo-reject.js";
+
+export default function handleProfileSignup(firstName, lastname, fileName) {
+    return Promise.allSettled([
+        signupUser(firstName, lastName),
+        uploadPhoto(fileName),
+    ]).then((results) => results.map((result) => {
+        if (result.status === 'fulfilled') {
+            return {
+                status: result.status,
+                value: result.value,
+            };
+        }
+
+        return {
+            status: result.status,
+            value: result.reason,
+        };
+    }));
+}
