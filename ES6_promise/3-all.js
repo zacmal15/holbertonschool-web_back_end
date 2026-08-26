3518de7ca739f9bfe1014ff9b7ceac17f05c42e5
Zac <13089@holbertonstudents.com.au>
2026-08-26 15:16:00 +1000

    fix

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,7 +3,7 @@ import { uploadPhoto, createUser } from './utils.js';
 export default function handleProfileSignup() {
     return Promise.all([uploadPhoto(), createUser()])
       .then(([photo, user]) => {
-        console.log(uploadPhoto.body, user.firstName, user.lastName);
+        console.log(photo.body, user.firstName, user.lastName);
       })
       .catch(() => {
         console.log('Signup system offline');
