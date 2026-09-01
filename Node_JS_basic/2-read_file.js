914fdb13af848585a571c00344b1fbf46b63ccb6
Zac <13089@holbertonstudents.com.au>
2026-09-01 16:14:11 +1000

    fix

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -30,7 +30,8 @@ function countStudents(path) {
 
     Object.keys(fields).forEach((field) => {
         console.log(
-            `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`,
+            `Number of students in ${field}: ${fields[field].length}. 
+            List: ${fields[field].join(', ')}`,
         );
     });
 }
