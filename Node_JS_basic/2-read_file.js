3c139785d63a4b2b24e121c780002c335c99d386
Zac <13089@holbertonstudents.com.au>
2026-09-01 16:08:04 +1000

    added next file

---
+++ Node_JS_basic/2-read_file.js
@@ -0,0 +1,38 @@
+const fs = require('fs');
+
+function countStudents(path) {
+    let data;
+
+    try {
+        data = fs.readFileSync(path, 'utf8');
+    }   catch (error) {
+        throw new Error('Cannot load the database');
+    }
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1);
+
+    console.log(`Number of students: ${students.length}`);
+
+    const fields = {};
+
+    students.forEach((student) => {
+        const values = student.split(',');
+        const firstName = values[0];
+        const field = values[3].trim();
+
+        if (!fields[field]) {
+            fields[field] = [];
+        }
+
+        fields[field].push(firstName);
+    });
+
+    Object.keys(fields).forEach((field) => {
+        console.log(
+            `Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`,
+        );
+    });
+}
+
+module.exports = countStudents;
