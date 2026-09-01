0c85c23f227e0c2c73f72bae3b920857285b7f10
Zac <13089@holbertonstudents.com.au>
2026-09-01 15:52:08 +1000

    fixed syntax

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -2,7 +2,7 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
     const name = data.toString().trim();
-    process.stdout.write('Your name is: ${name}\n');
+    process.stdout.write(`Your name is: ${name}\n`);
 });
 
 process.stdin.on('end', () => {
